09c6b575d34a47de18c58bfcb9d05bd27831b395
Erwin Rieger <er-ri@gmx.net>
2015-07-24 23:45:27 +0200

    Fixed compiler warnings for 64 bit simulator build.

--- firmware/ddprint.cpp
+++ firmware/ddprint.cpp
@@ -116,20 +116,18 @@ uint8_t errorFlags = 0;
 uint8_t Stopped = 0;
 
 
-
-
 extern "C"{
   extern unsigned int __bss_end;
   extern unsigned int __heap_start;
-  extern void *__brkval;
+  extern unsigned int *__brkval;
 
   int freeMemory() {
-    int free_memory;
+    unsigned int free_memory;
 
-    if((int)__brkval == 0)
-      free_memory = ((int)&free_memory) - ((int)&__bss_end);
+    if(__brkval == 0)
+      free_memory = (&free_memory) - (&__bss_end);
     else
-      free_memory = ((int)&free_memory) - ((int)__brkval);
+      free_memory = (&free_memory) - (__brkval);
 
     return free_memory;
   }
