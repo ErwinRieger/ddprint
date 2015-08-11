fcd07f54e75c815ec0b3b70df98281ff44cf9cb7
Erwin Rieger <er-ri@gmx.net>
2015-08-11 19:42:51 +0200

    Fixed path of testfile.

--- host/test_big.sh
+++ host/test_big.sh
@@ -1,7 +1,7 @@
 ./test.sh \
     ~/elektronik/3dmodels/downloads/20140926_Marvin_KeyChain.gcode \
     ~/elektronik/3dmodels/downloads/UltimakerRobot_support.gcode \
-    /home/erwin/elektronik/3dmodels/halter_um2_frontscheibe/halter.gcode \
+    /home/erwin/elektronik/3dmodels/um/halter_um2_frontscheibe/halter.gcode \
     ~/elektronik/3dmodels/lüftung/adapter_duese.gcode \
     ~/elektronik/3dmodels/klammer_schwimmbad/klammer.gcode \
     ~/elektronik/3dmodels/buchse_federbein_fahrrad/buchse.gcode \
