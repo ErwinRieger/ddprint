437459b1fbf1162f42d5d9ee525172aa5132feea
Erwin Rieger <er-ri@gmx.net>
2016-12-15 12:13:59 +0100

    Added kermit ccitt crc16 for simulator.

--- firmware/simulator/util/crc16.h
+++ firmware/simulator/util/crc16.h
@@ -17,3 +17,12 @@ static uint16_t _crc_xmodem_update (uint16_t crc, uint8_t data) {
     return crc;
 }
 
+static uint16_t _crc_ccitt_update (uint16_t crc, uint8_t data) {
+
+    data ^= crc & 0xFF;
+    data ^= data << 4;
+
+    return ((((uint16_t)data << 8) | (crc >> 8)) ^ (uint8_t)(data >> 4) 
+            ^ ((uint16_t)data << 3));
+}
+
