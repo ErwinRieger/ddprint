ce1dabd622ef97e0eba249ab003dc6a0b3239fd2
Erwin Rieger <er-ri@gmx.net>
2016-12-30 17:53:33 +0100

    Cleanup, cura-path, machine and material profile per command line parameter now.

--- scripts/cura-engine-settings.sh
+++ scripts/cura-engine-settings.sh
@@ -1,10 +1,24 @@
+#!/usr/bin/env bash
+#
+# Usage:
+#
+#   cura-engine-settings.sh <cura-dir> <machine-profile> <mat-profile> <input-stl> <output-gcode> | xargs -0 /opt/cura/bin/CuraEngine
+#
 
-export PYTHONPATH="/opt/cura/lib/python3/dist-packages"
-export LD_LIBRARY_PATH=/opt/cura/lib
+# XXX adjust to your cura installation dir:
+CURADIR="$1"
+machProfile="$2"
+matProfile="$3"
+inputFile="$4"
+outputFile="$5"
+
+export PYTHONPATH="${CURADIR}/lib/python3/dist-packages"
+export LD_LIBRARY_PATH="${CURADIR}/lib"
 
 dn=$(dirname $0)
 
-echo -ne "slice\0-v\0-j\0/opt/cura/share/cura/resources/definitions/fdmprinter.def.json\0"
-$dn/ce-settings.py /opt/cura "Ultimaker 2 0.80" "advance_08" 2>/dev/null
-echo -ne "-s\0center_obj=true\0-o\0$2\0-l\0$1\0"
+# echo -ne "slice\0-v\0-j\0${CURADIR}/share/cura/resources/definitions/fdmprinter.def.json\0"
+echo -ne "slice\0-j\0${CURADIR}/share/cura/resources/definitions/fdmprinter.def.json\0"
+$dn/cura-engine-settings.py ${CURADIR} "Ultimaker 2 0.80" "advance_08" 2>/dev/null
+echo -ne "-s\0center_object=true\0-o\0${outputFile}\0-l\0${inputFile}\0"
 
