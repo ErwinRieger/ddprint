b26e73da8171860b69778a2c887f783dcea7322e
Erwin Rieger <er-ri@gmx.net>
2017-01-02 10:42:42 +0100

    Override slicer settings per commandline.

--- scripts/cura-engine-wrapper.sh
+++ scripts/cura-engine-wrapper.sh
@@ -10,11 +10,12 @@ machProfile="$2"
 matProfile="$3"
 inputFile="$4"
 outputFile="$5"
+additionalSettings="$6"
 
 dn="$(dirname $0)"
 
 export PYTHONPATH="${curaDir}/lib/python3/dist-packages"
 export LD_LIBRARY_PATH="${curaDir}/lib"
 
-$dn/cura-engine-settings.py ${curaDir} "Ultimaker 2 0.80" "advance_08" ${inputFile} ${outputFile}
+$dn/cura-engine-wrapper.py "${curaDir}" "Ultimaker 2 0.80" "advance_08" "${inputFile}" "${outputFile}" "${additionalSettings}"
 
