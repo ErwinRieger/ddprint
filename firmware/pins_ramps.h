8dedeee30685ce7cf5eeae70dfbc17625704bdbe
Erwin Rieger <er-ri@gmx.net>
2017-02-18 09:13:03 +0100

    Cleanup: removed unused code.

--- firmware/pins_ramps.h
+++ firmware/pins_ramps.h
@@ -79,6 +79,7 @@
 
     #define LED_PIN            13
 
+    // #error 770
     #define FAN_PIN            9 // (Sprinter config)
 
   #define PS_ON_PIN          12
@@ -93,22 +94,12 @@
     #define HEATER_1_PIN       -1
   #define HEATER_2_PIN       -1
 
-  #if MOTHERBOARD == 77
-    #define HEATER_0_PIN       10
-    #define HEATER_1_PIN       12
-    #define HEATER_2_PIN       6
-  #endif
-
   #define TEMP_0_PIN         13   // ANALOG NUMBERING
   // xxx messed up wiring, T1 and T2 swapped
   // #define TEMP_1_PIN         15   // ANALOG NUMBERING
   #define TEMP_1_PIN         14   // ANALOG NUMBERING
   #define TEMP_2_PIN         -1   // ANALOG NUMBERING
-  #if MOTHERBOARD == 77
-    #define HEATER_BED_PIN     9    // BED
-  #else
-    #define HEATER_BED_PIN     8    // BED
-  #endif
+  #define HEATER_BED_PIN     8    // BED
   // xxx messed up wiring, T1 and T2 swapped
   // #define TEMP_BED_PIN       14   // ANALOG NUMBERING
   #define TEMP_BED_PIN       15   // ANALOG NUMBERING
@@ -175,22 +166,6 @@
 
       #endif
 
-      #if MOTHERBOARD == 77
-        #define BEEPER -1
-
-        #define LCD_PINS_RS 27
-        #define LCD_PINS_ENABLE 29
-        #define LCD_PINS_D4 37
-        #define LCD_PINS_D5 35
-        #define LCD_PINS_D6 33
-        #define LCD_PINS_D7 31
-
-       //buttons
-       #define BTN_EN1 16
-       #define BTN_EN2 17
-       #define BTN_ENC 23 //the click
-
-    #endif
     #else //old style panel with shift register
       //arduino pin witch triggers an piezzo beeper
       #define BEEPER 33		//No Beeper added
