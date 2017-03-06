5f94977f1248b01979cfbdb2baadfd38837df601
Erwin Rieger <er-ri@gmx.net>
2017-03-06 08:22:49 +0100

    Decreased BED_MAXTEMP from 200 to 150.

--- firmware/Configuration.h
+++ firmware/Configuration.h
@@ -97,7 +97,7 @@
 #define HEATER_0_MAXTEMP 285
 #define HEATER_1_MAXTEMP 285
 #define HEATER_2_MAXTEMP 285
-#define BED_MAXTEMP 200
+#define BED_MAXTEMP 150
 
 //Check if the heater heats up MAX_HEATING_TEMPERATURE_INCREASE within MAX_HEATING_CHECK_MILLIS while the PID was at the maximum.
 // If not, raise an error because most likely the heater is not heating up the temperature sensor. Indicating an issue in the system.
