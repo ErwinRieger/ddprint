679ef9f71fac58e674a031781f6482561565ee2f
Erwin Rieger <er-ri@gmx.net>
2018-02-03 16:59:03 +0100

    Added more test gcode files.

--- scripts/parserTest-big.sh
+++ scripts/parserTest-big.sh
@@ -1,9 +1,3 @@
-exec $(dirname $0)/parserTest.sh \
-    ~/elektronik/3dmodels/downloads/20140926_Marvin_KeyChain.gcode \
-    ~/elektronik/3dmodels/downloads/UltimakerRobot_support.gcode \
-    /home/erwin/elektronik/3dmodels/um/halter_um2_frontscheibe/halter.gcode \
-    ~/elektronik/3dmodels/lüftung/adapter_duese.gcode \
-    ~/elektronik/3dmodels/klammer_schwimmbad/klammer.gcode \
-    ~/elektronik/3dmodels/buchse_federbein_fahrrad/buchse.gcode \
-    ~/elektronik/3dmodels/downloads/UM2_-_Extrusion_Test_-_NoRet_-_3-10mm3.gcode
+
+exec $(dirname $0)/parserTest.sh $(ls test_files/big/*.gcode)
 
