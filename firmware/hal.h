8fc12f5c1bd7d7cb398be42f70a109c05c63c7e4
ErwinRieger <erwin.rieger@ibrieger.de>
2020-07-10 17:53:14 +0200

    Porting JennyPrinter: added HAL include files.

---
+++ firmware/hal.h
@@ -0,0 +1,31 @@
+/*
+* This file is part of ddprint - a direct drive 3D printer firmware.
+* 
+* Copyright 2020 erwin.rieger@ibrieger.de
+* 
+* ddprint is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* ddprint is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with ddprint.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#if defined(__avr__)
+    // ATMega 2560
+    #include "hal_avr.h"
+#endif
+
+#if defined(__arm__)
+    // STM32
+    #include "hal_arm.h"
+#endif
+
---
+++ firmware/hal_arm.h
@@ -0,0 +1,30 @@
+/*
+* This file is part of ddprint - a direct drive 3D printer firmware.
+* 
+* Copyright 2020 erwin.rieger@ibrieger.de
+* 
+* ddprint is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* ddprint is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with ddprint.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//
+// STM32
+//
+// Note: fixed USART1 usage
+//
+
+#define SERIAL_TX_DR_EMPTY() ( USART1->regs->SR & USART_SR_TXE ) 
+
+#define SERIAL_TX_DR_PUTC(c) ( USART1->regs->DR = c )
+
+
