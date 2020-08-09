49ac9a9f59b88cea9094f3c494da1bbfcae24586
ErwinRieger <erwin.rieger@ibrieger.de>
2020-08-09 09:09:48 +0200

    Added common massstorage base class header massstoragebase.h.

---
+++ firmware/massstoragebase.h
@@ -0,0 +1,35 @@
+/*
+* This file is part of ddprint - a direct drive 3D printer firmware.
+* 
+* Copyright 2020 erwin.rieger@ibrieger.de
+* 
+* ddprint is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* ddprint is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with ddprint.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// Buffer and sector size reading and writing to the storage device
+#define SwapSectorSize 512
+
+//
+// Common base class for MassStorage classes
+//
+
+class MassStorageBase {
+
+  protected:
+
+    uint8_t writeBuffer[SwapSectorSize];
+};
+
