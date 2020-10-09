32cf3e9641081137f75a6899656e2ee5b9fa7339
ErwinRieger <erwin.rieger@ibrieger.de>
2020-10-09 21:40:41 +0200

    Added unzip library.

---
+++ firmware/src/unzip/tinf_compat.h
@@ -0,0 +1,8 @@
+/* This header contains compatibility defines for the original tinf API
+   and uzlib 2.x and below API. These defines are deprecated and going
+   to be removed in the future, so applications should migrate to new
+   uzlib API. */
+#define TINF_DATA struct uzlib_uncomp
+
+#define destSize dest_size
+#define readSource source_read_cb
