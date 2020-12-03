222c3827c758e29e097432894ca295aab141fca9
ErwinRieger <erwin.rieger@ibrieger.de>
2020-12-03 21:52:04 +0100

    Minor fixes of debug traces.

--- host/ddprinter.py
+++ host/ddprinter.py
@@ -264,8 +264,8 @@ class Printer(Serial):
                 message = payload[2:2+slen]
                 self.gui.logComm("GenericMessage: '%s'" % message)
             elif msgType == BufDebug:
-                (p0, p1, p2, swapdev, sdreader) = struct.unpack("<IIiII", payload[1:])
-                self.gui.logComm("BufDebug: swapdev: %d, sdreader: %d (p0: %d, p1: %d, p2: %d)" % (swapdev, sdreader, p0, p1, p2))
+                (swapdev, sdreader) = struct.unpack("<II", payload[1:])
+                self.gui.logComm("BufDebug: swapdev: %d, sdreader: %d" % (swapdev, sdreader))
             return True # consume message
 
         return False # continue message processing
@@ -712,7 +712,7 @@ class Printer(Serial):
 
         (cmd, payload) = self.query(CmdGetStatus, doLog=False)
 
-        print "len payload:", len(payload)
+        # print "len payload:", len(payload)
 
 
         # XXXXXX debug workaround, payload 37, needed: 36
@@ -737,12 +737,12 @@ class Printer(Serial):
 
         self.gui.statusCb(statusDict)
 
-        for (statusCmd, tasknames) in [(CmdGetTaskStatus, ("idle", "tempcontrol", "tempheater", "filsensor", "ubscommand", "txbuffer", "swapdev", "fillbuffer", "tasksum")), (CmdGetIOStats, ("read", "write", "writeSum"))]:
+        for (statusCmd, statusName, tasknames) in [(CmdGetTaskStatus, "TOP:", ("idle", "tempcontrol", "tempheater", "filsensor", "ubscommand", "txbuffer", "swapdev", "fillbuffer", "tasksum")), (CmdGetIOStats, "IOStats:", ("read", "readSum", "write", "writeSum"))]:
 
             ## XXX debug iotimings
             (cmd, payload) = self.query(statusCmd, doLog=True)
 
-            print "cmd, len payload:", cmd, len(payload)
+            # print "cmd, len payload:", cmd, len(payload)
 
             lenp =(len(payload) / 4) * 4;
 
@@ -755,6 +755,7 @@ class Printer(Serial):
             tup = struct.unpack(structFmt, payload)
 
             tupindex = 0
+            print statusName
             print "%15s %10s %10s %10s" % ("Task", "#Calls", "TSum", "TLongest")
             for taskname in tasknames:
                 nCalls = tup[tupindex]
@@ -770,8 +771,8 @@ class Printer(Serial):
         slipstr = "----"
         if statusDict["slippage"]:
             slipstr = "%4.2f" % (1.0/statusDict["slippage"])
-        print "State: %d, Bed: %5.1f, Hotend: %5.1f(%5.1f), Pwm: %3d, Swap: %10d, Underrun: %5d, Grip: %s" % \
-            (statusDict["state"], statusDict["t0"], statusDict["t1"], statusDict["targetT1"], statusDict["pwmOutput"], statusDict["Swap"], statusDict["StepBufUnderRuns"], slipstr)
+        print "State: %d, Bed: %5.1f, Hotend: %5.1f(%5.1f), Pwm: %3d, Swap: %10s, Underrun: %5d, Grip: %s" % \
+            (statusDict["state"], statusDict["t0"], statusDict["t1"], statusDict["targetT1"], statusDict["pwmOutput"], util.sizeof_fmt(statusDict["Swap"]), statusDict["StepBufUnderRuns"], slipstr)
 
     # Get printer (-profile) name from printer eeprom
     def getPrinterName(self):
