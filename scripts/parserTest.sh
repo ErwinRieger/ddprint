0907a240e035639fc884033c3640c89f965bafcf
ErwinRieger <erwin.rieger@ibrieger.de>
2020-12-18 10:59:46 +0100

    Use lower case command line args.

--- scripts/parserTest.sh
+++ scripts/parserTest.sh
@@ -9,12 +9,12 @@ for inp in $(ls -rS $* test_files/*.gcode); do
 
     for kadv in 0 0.1 0.4 0.5 1; do
 
-        echo "python ddprint.py -kAdvance $kadv pre UM2_profile_template nozzle40 pla_1.75mm $inp"
-        python ddprint.py -kAdvance $kadv pre UM2_profile_template nozzle40 pla_1.75mm $inp
+        echo "python ddprint.py -kadvance $kadv pre UM2_profile_template nozzle40 pla_1.75mm $inp"
+        python ddprint.py -kadvance $kadv pre UM2_profile_template nozzle40 pla_1.75mm $inp
 
         if [ "$?" != "0" ]; then
             echo "########################################################"
-            echo "$0: error running Test $nTests $inp with kAdvance $kadv"
+            echo "$0: error running Test $nTests $inp with kadvance $kadv"
             echo "########################################################"
             echo "$kadv $inp" > /tmp/parserTest_error_file
             exit 1
