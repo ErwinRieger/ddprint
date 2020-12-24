36498cc898a8f239f6d44f3b8272e816f7ac7838
ErwinRieger <erwin.rieger@ibrieger.de>
2020-12-24 01:03:22 +0100

    Cleanup,rework parsertest.sh.

--- scripts/parserTest.sh
+++ scripts/parserTest.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
 
 nTests=0
-for inp in $(ls -rS $* test_files/*.gcode); do 
+
+for inp in $*; do 
 
     echo "###################################"
     echo "$0: running python ddprint.py pre $inp" >&2
     echo "###################################"
 
-    for kadv in 0 0.1 0.4 0.5 1; do
+    # for kadv in 0 0.1 0.4 0.5 1; do
+    for kadv in 0.4 0.5 1 0 0.1; do
 
-        echo "python ddprint.py -kadvance $kadv pre UM2_profile_template nozzle40 pla_1.75mm $inp"
-        python ddprint.py -kadvance $kadv pre UM2_profile_template nozzle40 pla_1.75mm $inp
+        echo "python -u ddprint.py -kadvance $kadv pre UM2_profile_template nozzle40 pla_1.75mm $inp"
+        python -u ddprint.py -kadvance $kadv pre UM2_profile_template nozzle40 pla_1.75mm $inp
 
         if [ "$?" != "0" ]; then
             echo "########################################################"
