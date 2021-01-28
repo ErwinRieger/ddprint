d9a19cbb02d1b8968520bc519914a46865823fa3
ErwinRieger <erwin.rieger@ibrieger.de>
2021-01-29 00:39:42 +0100

    Minor improvements to gdb script.

--- scripts/rungdb.sh
+++ scripts/rungdb.sh
@@ -2,7 +2,7 @@
 echo ""
 echo "Note: be aware of the watchdog, it may be enabled in the firmware."
 echo ""
-st-util &
+pidof st-util || st-util &
 sleep 1
 arm-none-eabi-gdb -ix ../../ddprint_stm32/examples/stm32_gdbinit build/stm32duino.STM32F4.generic_f407v/firmware.ino.elf
 
