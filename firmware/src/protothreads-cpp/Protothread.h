7bca6b661f10a30ec9fd930a7a41231754f48e96
ErwinRieger <erwin.rieger@ibrieger.de>
2022-01-20 09:08:56 +0100

    Made Run() method non-virtual to save the overhead of virtual functions, not needed for ddPrint.

--- firmware/src/protothreads-cpp/Protothread.h
+++ firmware/src/protothreads-cpp/Protothread.h
@@ -78,7 +78,12 @@ public:
     // Run next part of protothread or return immediately if it's still
     // waiting. Return true if protothread is still running, false if it
     // has finished. Implement this method in your Protothread subclass.
-    virtual bool Run() = 0;
+    //
+    // Note: changed abstract virtual Run() method to normal method to
+    // save the overhead of the virtual call.
+    //
+    // virtual bool Run() = 0;
+    bool Run();
 
 protected:
     // Used to store a protothread's position (what Dunkels calls a
