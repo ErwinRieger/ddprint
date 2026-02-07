861a06b6d1355a6ec3e78551c544e2cb82501c42
ErwinRieger <erwin.rieger@ibrieger.de>
2026-02-07 18:49:39 +0100

    Ignore ddconfig.py when calculating word count.

--- scripts/getversion.sh
+++ scripts/getversion.sh
@@ -1,2 +1,2 @@
-wc -w $DDPRINTHOME/host/*.py $DDPRINTHOME/firmware/*.cpp $DDPRINTHOME/firmware/*.h $DDPRINTHOME/firmware/pins/*.h|tail -1|(read n t; echo $n)
+wc -w $(ls $DDPRINTHOME/host/*.py|grep -v ddconfig.py) $DDPRINTHOME/firmware/*.cpp $DDPRINTHOME/firmware/*.h $DDPRINTHOME/firmware/pins/*.h|tail -1|(read n t; echo $n)
 
